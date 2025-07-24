b950dbc82ac3ad273c001cd5942e6ca0d965199c
dhoni <muh.dhoni@gmail.com>
2025-07-24 23:29:30 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cv-generator-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
