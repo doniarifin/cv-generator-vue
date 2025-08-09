93e29df3b7e681589a097b03032e933f9fc8f49e
Dhoni <muh.dhoni@gmail.com>
2025-08-10 02:17:58 +0700

    fix: basepath

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cv-generator-vue/',
+  base: '/vue-cvgenerator/',
   plugins: [vue()],
   resolve: {
     alias: {
